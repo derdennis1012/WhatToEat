60677a171dff059247e7b1022d3c9a2904a696b5
Dennis Bölling <derdennis1012@gmail.com>
2023-12-05 11:14:52 +0100

    Frontend

--- frontend/src/main.js
+++ frontend/src/main.js
@@ -1,5 +1,13 @@
 import { createApp } from 'vue'
+import { createRouter, createWebHistory } from 'vue-router'
+
 import './style.css'
 import App from './App.vue'
+const router = createRouter({
+    history: createWebHistory(),
+    routes: []
+  })
+  
+  app.use(router)
 
 createApp(App).mount('#app')
