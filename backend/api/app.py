5996ee8ece10343618930928b31032f1bcbc9e4d
Dennis Bölling <derdennis1012@gmail.com>
2023-12-18 08:21:16 +0100

    push

--- backend/api/app.py
+++ backend/api/app.py
@@ -3,7 +3,6 @@ from pymongo import MongoClient
 
 app = Flask(__name__)
 
-#uri mongodb://root:WaRa_bestSchool%40wrld!@localhost:27017/?authSource=admin
 MONGODB_URI = "mongodb://root:WaRa_bestSchool%40wrld!@localhost:27017/flask_db?authSource=admin"
 client = MongoClient(MONGODB_URI)
 
@@ -17,5 +16,3 @@ stores = db.stores
 @app.route("/")
 def index():
     return "Hello World"
-if __name__ == '__main__':
-    app.run(debug=True, port=5555)
